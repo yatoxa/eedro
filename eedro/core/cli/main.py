0aff97bde0763ad1ac34fa877709b14de609e592
Toxa Yantsen <tx.ntsn.cd@gmail.com>
2025-03-08 12:41:53 +0300

    - added main_cmd factory;

--- eedro/core/cli/main.py
+++ eedro/core/cli/main.py
@@ -1,40 +1,52 @@
 import os
 import pathlib
+from types import ModuleType
+from typing import Callable
 
 import click
 
-from ... import get_version
+import eedro
+
 from ...contrib.log import LogLevel, enable_console_log
 
 
-@click.group()
-@click.option(
-    "-w",
-    "--work-dir",
-    type=click.Path(
-        exists=True,
-        file_okay=False,
-        resolve_path=True,
-        path_type=pathlib.Path,
-    ),
-    default=pathlib.Path(os.getcwd()).resolve(),
-    show_default=True,
-    help="Set current working directory.",
-)
-@click.option(
-    "--log-level",
-    type=click.Choice(LogLevel.__members__, case_sensitive=False),
-    callback=lambda c, p, v: LogLevel.str_to_log_level(v),
-    default="INFO",
-    show_default=True,
-    help="Set logging level.",
-)
-@click.option(
-    "--console-log",
-    is_flag=True,
-    help="Enable logging to console.",
-)
-@click.version_option(version=get_version(), package_name="eedro")
-def main_cmd(*, console_log: bool = False, **options) -> None:
-    if console_log:
-        enable_console_log()
+def get_main_cmd(root_pkg: ModuleType) -> Callable:
+    @click.group()
+    @click.option(
+        "-w",
+        "--work-dir",
+        type=click.Path(
+            exists=True,
+            file_okay=False,
+            resolve_path=True,
+            path_type=pathlib.Path,
+        ),
+        default=pathlib.Path(os.getcwd()).resolve(),
+        show_default=True,
+        help="Set current working directory.",
+    )
+    @click.option(
+        "--log-level",
+        type=click.Choice(LogLevel.__members__, case_sensitive=False),
+        callback=lambda c, p, v: LogLevel.str_to_log_level(v),
+        default="INFO",
+        show_default=True,
+        help="Set logging level.",
+    )
+    @click.option(
+        "--console-log",
+        is_flag=True,
+        help="Enable logging to console.",
+    )
+    @click.version_option(
+        version=root_pkg.get_version(),
+        package_name=root_pkg.__name__,
+    )
+    def _main_cmd(*, console_log: bool = False, **options) -> None:
+        if console_log:
+            enable_console_log()
+
+    return _main_cmd
+
+
+main_cmd = get_main_cmd(eedro)
