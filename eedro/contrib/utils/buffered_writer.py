ae64134604870f7a64150ede0ec634db592da070
Toxa Yantsen <tx.ntsn.cd@gmail.com>
2026-02-15 17:39:53 +0100

    - added DelayedBufferedWriter and jitter to BufferedWriter, parameter new_line_separator renamed to newline;

--- eedro/contrib/utils/buffered_writer.py
+++ eedro/contrib/utils/buffered_writer.py
@@ -1,6 +1,9 @@
+import io
+import logging
 import pathlib
 import time
 from datetime import UTC, datetime
+from random import randint
 from typing import Callable, List, Optional, TextIO
 from uuid import uuid4
 
@@ -13,23 +16,31 @@ class BufferedWriter:
         max_size: int = 1_000_000,
         save_period_s: int | float = 900,
         initial_ts: Optional[float] = None,
+        max_jitter: int = 0,
         buffer_filename_prefix: Optional[str] = None,
         buffer_filename_timestamp_format: Optional[str] = None,
         buffer_filename_extension: str = ".data",
         save_data_handler: Optional[Callable[[TextIO], None]] = None,
-        new_line_separator: str = "\n",
+        newline: str = "\n",
     ) -> None:
         self._base_dir = base_dir
         self._max_size = max_size
         self._save_period_s = save_period_s
-        self._last_drain_ts = initial_ts or time.time()
+        assert (
+            max_jitter >= 0
+        ), "The max_jitter parameter must be greater than or equal to zero!"
+        self._max_jitter = max_jitter
+        self._last_drain_ts = (initial_ts or time.time()) + self.get_jitter()
         self._buffer_filename_prefix = buffer_filename_prefix
         self._buffer_filename_timestamp_format = buffer_filename_timestamp_format
         self._buffer_filename_extension = buffer_filename_extension
         self._save_data_handler = save_data_handler
-        self._new_line_separator = new_line_separator
+        self._newline = newline
         self._buffer_file: Optional[TextIO] = None
         self._counter = 0
+
+    def get_jitter(self) -> int:
+        return self._max_jitter and randint(0, self._max_jitter)
 
     def get_new_buffer_filename_timestamp(self) -> str:
         if self._buffer_filename_timestamp_format:
@@ -70,7 +81,7 @@ class BufferedWriter:
             self._buffer_file.close()
             self._buffer_file = None
             self._counter = 0
-            self._last_drain_ts = time.time()
+            self._last_drain_ts = time.time() + self.get_jitter()
 
     def _get_buffer(self) -> TextIO:
         self.drain_buffer()
@@ -80,23 +91,18 @@ class BufferedWriter:
 
         return self._buffer_file
 
-    def write(self, line: str, *, new_line_separator: Optional[str] = None) -> None:
-        if new_line_separator is None:
-            new_line_separator = self._new_line_separator
+    def write(self, line: str, *, newline: Optional[str] = None) -> None:
+        if newline is None:
+            newline = self._newline
 
-        self._get_buffer().write(line + new_line_separator)
+        self._get_buffer().write(line + newline)
         self._counter += 1
 
-    def writelines(
-        self,
-        lines: List[str],
-        *,
-        new_line_separator: Optional[str] = None,
-    ) -> None:
-        if new_line_separator is None:
-            new_line_separator = self._new_line_separator
+    def writelines(self, lines: List[str], *, newline: Optional[str] = None) -> None:
+        if newline is None:
+            newline = self._newline
 
-        self._get_buffer().writelines((line + new_line_separator for line in lines))
+        self._get_buffer().writelines((line + newline for line in lines))
         self._counter += len(lines)
 
     def __enter__(self) -> "BufferedWriter":
@@ -104,3 +110,18 @@ class BufferedWriter:
 
     def __exit__(self, exc_type, exc_val, exc_tb) -> None:
         self.drain_buffer(force=True)
+
+
+class DelayedBufferedWriter(BufferedWriter):
+    def get_new_buffer_file(self) -> TextIO:
+        return io.StringIO()
+
+    def save_data(self, buffer_file: TextIO) -> None:
+        with super().get_new_buffer_file() as output_file:
+            output_file.writelines(line for line in buffer_file)
+
+            logging.info(
+                "Buffered data saved to file %s | %d rows",
+                pathlib.Path(output_file.name).resolve(),
+                self._counter,
+            )
